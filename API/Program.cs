65aa43f0e08ba34263824c36c1063244809f7c3d
Sascha Heimann <madcoda9000@users.noreply.github.com>
2022-12-27 10:51:22 +0100

    27.12.2022 10:51

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,18 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("default"));
-});
-
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -19,6 +20,9 @@ var app = builder.Build();
 app.UseCors(
     policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
 );
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
